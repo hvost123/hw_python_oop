e347f67567599e084736b7dc3118eec1bea7dc6a
Evgeniy <zinovev.evgenii@mail.ru>
2023-04-16 20:43:06 +0200

    run10

--- homework.py
+++ homework.py
@@ -89,7 +89,6 @@ class SportsWalking(Training):
     INDEX_1: float = 0.035
     INDEX_2: float = 0.029
     KMH_IN_MS: float = 0.278
-    IN_M = 100
 
     def __init__(self,
                  action: int,
@@ -101,10 +100,10 @@ class SportsWalking(Training):
         self.height = height
 
     def get_spent_calories(self) -> float:
-        calories = ((self.INDEX_1 * self.weight + (self.get_mean_speed()
-                    * self.KMH_IN_MS**2 / self.height / self.M_IN_KM)
+        calories = (((self.INDEX_1 * self.weight + ((self.get_mean_speed()
+                    * self.KMH_IN_MS)**2 / self.height / self.M_IN_KM)
                     * self.INDEX_2
-                    * self.weight) * (self.duration * self.H_IN_MIN))
+                    * self.weight) * self.duration * self.H_IN_MIN))
         return calories
 
 
